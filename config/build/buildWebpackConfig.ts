baae5933e5369f443c62012116d21264aa06e3ea
Eugene <popolamov908gmail.com>
2025-06-24 16:09:35 +0300

    decompsition webpack

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import { Configuration } from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths, isDev } = options;
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      path: paths.build,
+      filename: "[name].[contenthash].bundle.js",
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+}
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,4 @@
 export function someFn(arg: number): string {
+  console.log("string");
   return "string";
 }
