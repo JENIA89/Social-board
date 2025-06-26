551ae896fc3ab56df40559a62fd9dd1f599b3be4
Evgene <popolamov908@gmail.com>
2025-06-26 22:33:24 +0300

    decomposed routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,9 @@
-
+import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProviders/lib/useTheme";
 import "./styles/index.scss";
+import { AppRouter } from "./providers/router";
+
 
 
 const App = () => {
@@ -9,8 +11,10 @@ const App = () => {
   return (
     <div>
       <div className={classNames("app", {}, [theme])}>
-        <h2>Главная</h2>
         <button onClick={toggleTheme}>Toggle theme</button>
+        <Link to={'/'}>Главная</Link>
+        <Link to={'/about'}>О сайте</Link>
+        <AppRouter />
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./app/App";
 import { ThemeProvider } from "app/providers/ThemeProviders";
+import { BrowserRouter } from 'react-router-dom';
 
-
-render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
-  document.getElementById("root")
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
+
+
