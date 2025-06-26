a2d9c7c15b4767e6d3aeb083feb09d5d84193d2e
Evgene <popolamov908@gmail.com>
2025-06-26 15:42:14 +0300

    add fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
-import { classNames } from "./helpers/classNames/classNames";
+
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProviders/lib/useTheme";
 import "./styles/index.scss";
-import { useTheme } from "./theme/useTheme";
+
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
