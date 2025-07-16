94f9c8655f62d42517dd1eba234585bd42ec5169
Evgene <popolamov908@gmail.com>
2025-07-16 16:03:36 +0300

    hide line button test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import {render, screen} from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Button, ThemeButton } from "./Button";
+import { Button, ThemeButton } from './Button';
 
-
-describe("Button", () => {
-    test("Test render with text", () => {
-        render(<Button>Text</Button>)
+describe('Button', () => {
+    test('Test render with text', () => {
+        // eslint-disable-next-line i18next/no-literal-string
+        render(<Button>Text</Button>);
         expect(screen.getByText('Text')).toBeInTheDocument();
     });
 
-    test("Test render with class", () => {
-        render(<Button theme={ThemeButton.CLEAR}>Text</Button>)
-        expect(screen.getByText('Text')).toHaveClass("clear");
-        screen.debug()
+    test('Test render with class', () => {
+        // eslint-disable-next-line i18next/no-literal-string
+        render(<Button theme={ThemeButton.CLEAR}>Text</Button>);
+        expect(screen.getByText('Text')).toHaveClass('clear');
+        screen.debug();
     });
 });
